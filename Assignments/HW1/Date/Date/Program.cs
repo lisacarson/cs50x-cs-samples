604d4229e40141da129cb816238649d28d57fab0
Lisa Carson <lcarson35@hotmil.com>
2015-09-19 11:25:53 -0400

    Update Date

---
+++ Assignments/HW1/Date/Date/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Date
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime currentTime = DateTime.Now;
+            Console.WriteLine(currentTime.ToShortDateString());
+            Console.WriteLine(currentTime.ToLongDateString());
+            Console.WriteLine(currentTime.ToShortTimeString());
+            Console.WriteLine(currentTime.ToLongTimeString());
+            Console.ReadLine();
+        }
+    }
+}
