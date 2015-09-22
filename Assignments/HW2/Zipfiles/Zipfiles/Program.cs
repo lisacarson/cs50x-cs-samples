a0d1e74de5c16d79cb4c0bf1c5100c4769448dda
Lisa Carson <lcarson35@hotmil.com>
2015-09-22 16:35:52 -0400

    Update HW

---
+++ Assignments/HW2/Zipfiles/Zipfiles/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zipfiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sourcefolder = "impoprtant";
+            var backfolder = "backup";
+            CreateZipBackup(sourcefolder, backfolder);
+        }
+        private static void CreateZipBackup(string folderToBackup, string destinationFolder, string filename = "backup")
+        {
+            if (!Directory.Exists(folderToBackup)) throw new DirectoryNotFoundException(folderToBackup + "does not exist");
+            if (!Directory.Exists(destinationFolder)) Directory.CreateDirectory(destinationFolder);
+
+            var backupName = filename + DateTime.Now.ToString("yy-MM-dd") + ".zip";
+            var backupLocation = Path.Combine(destinationFolder, backupName);
+
+            using (FileStream zipToOpen = File.Open(backupLocation, FileMode.Create))
+            {
+                using (ZipArchive archive = new ZipArchive(zipToOpen, ZipArchiveMode.Update))
+                {
+                    foreach (var file in Directory.EnumerateFiles(folderToBackup))
+                    {
+                        archive.CreateEntryFromFile(file, Path.GetFileName(file));
+                    }
+                }
+            }
+        }
+    }
+}
--- Assignments/HW2/Zipfiles/Zipfiles/Properties/AssemblyInfo.cs
+++ Assignments/HW2/Zipfiles/Zipfiles/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Zip")]
+[assembly: AssemblyTitle("Zipfiles")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Zip")]
+[assembly: AssemblyProduct("Zipfiles")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6b0bd1b7-55ff-45e6-9706-807e4c68d9dc")]
+[assembly: Guid("87bcc7b6-1911-49bd-80bc-5455f6fa2db6")]
 
 // Version information for an assembly consists of the following four values:
 //
